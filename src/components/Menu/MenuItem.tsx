9b3e364182bf6164e36fcc698477041bb4d6d64e
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-12-23 19:00:31 +0530

    chore: removed unwanted warnings

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback } from "react";
 import TagsInput from "react-tagsinput";
 import "react-tagsinput/react-tagsinput.css";
